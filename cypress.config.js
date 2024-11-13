ff9c2e6beb61d62cf24d2e26de5ef491703b5bde
Rodrigo Bonone Moreira <rbonone@gmail.com>
2024-11-13 16:35:06 -0300

    Add mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,23 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/reports/junit/results-[hash].xml'
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       on('file:preprocessor', cucumber())
     },
     specPattern: "cypress/e2e/step_definitions/*.feature"
